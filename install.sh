b36fa5dd3afda49ba6a31889648d4c7140b21bb9
Martim <mitram790@gmail.com>
2023-01-31 19:00:15 +0000

    Install script now when folders don't exist

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
+#!/bin/bash
 git clone --bare https://github.com/MartimVideira/dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
+
+# Function that move file to target destination, if folders in path do not exist
+# it creates them
+function mvp {
+    local source=$1
+    local destination=$2
+    mv $source $destination > /dev/null 2>$1
+    if [ $? = 1 ]; then
+        echo "Inside if"
+        dirname $destination | xargs -I{} mkdir -p {}; mv $source $destination
+        echo "Created Directories"
+    fi;
+}
+export -f mvp
 mkdir -p .config-backup
 
 # Actually Loads workspace -> we were on a barerepository
-
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    echo "Backing up pre-existing dot files."
+    config checkout  2>&1 | egrep "^\s\S+$" | awk {'print $1'} |  xargs -I {} bash -c 'mvp "$@" .config-backup/"$@"' {} {}
 fi;
 config checkout
 config config status.showUntrackedFiles no
